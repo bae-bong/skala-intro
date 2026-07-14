4c27ad1da94d535178a9b4ea7e819aafe99e1a07
배은빈 <baeunbin@naver.com>
2026-07-14 16:31:11 +0900

    Echo 프로그램 코드 개선하기

---
+++ echo.py
@@ -0,0 +1,9 @@
+# 사용자 입력을 받아 그대로 출력하는 프로그램
+def main():
+    user_input = input("문장을 입력하세요: ") # 사용자 입력 받기
+    print("입력한 문장은:", user_input) # 입력받은 문장 출력
+
+if __name__ == "__main__":
+    main()
+
+
