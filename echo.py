2a33e6f94bdaae46b894a88e57acffaa3d024272
배은빈 <baeunbin@naver.com>
2026-07-14 16:41:23 +0900

    Echo 프로그램 코드 개선하기

--- echo.py
+++ echo.py
@@ -1,7 +1,13 @@
 # 사용자 입력을 받아 그대로 출력하는 프로그램
 def main():
-    user_input = input("문장을 입력하세요: ") # 사용자 입력 받기
-    print("입력한 문장은:", user_input) # 입력받은 문장 출력
+    while True: # 무한 루프를 시작합니다.
+        user_input = input("문장을 입력하세요(!quit 입력 시 종료): ") # 사용자 입력 받기
+
+        if user_input == "!quit":   # 종료 조건 확인
+            print("프로그램을 종료합니다.")
+            break   # 루프를 종료하고 프로그램을 끝냅니다.
+
+        print("입력한 문장은:", user_input) # 입력받은 문장 출력
 
 if __name__ == "__main__":
     main()
